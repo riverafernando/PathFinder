0b6a89c508b02f56d2092267a5e77e4e8d8ef150
riverafernando <88053636+riverafernando@users.noreply.github.com>
2021-07-28 16:47:39 -0400

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -10,7 +10,7 @@ public class Frame
     public static void main(String [] args)
     {
         JFrame p = new JFrame();
-        p.setTitle("Seeker");
+        p.setTitle("PathFinder");
         p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
         p.setLayout(new GridLayout(1,1,0,0));    
         p.setResizable(true);
@@ -18,7 +18,6 @@ public class Frame
         p.pack();
         p.setLocationRelativeTo(null);
         p.setVisible(true);
-        p.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Fernando\\Desktop\\pixil-frame-03.png"));
-
+       
     }
 }
